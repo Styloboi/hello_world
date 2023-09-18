f6ada69753d697529abb90b72e336ea4f2f27b43
Styloboi <alexisdbr051@gmail.com>
2023-09-18 18:16:14 +0000

    A Crazy Loop
    Crazy?

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main{
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        for (int i = 0; i > 5; i++){
+            System.out.println("Crazy? I was crazy once. They put me in a rubber room. A rubber room with rats. And rats make me crazy");
+        }
     }
 }
